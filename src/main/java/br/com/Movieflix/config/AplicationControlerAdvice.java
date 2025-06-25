752db9f9f5907c837d7eedd1049346487489e2a8
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-06-25 14:25:39 -0300

    add exceptions

--- src/main/java/br/com/Movieflix/config/AplicationControlerAdvice.java
+++ src/main/java/br/com/Movieflix/config/AplicationControlerAdvice.java
@@ -2,15 +2,31 @@ package br.com.Movieflix.config;
 
 import br.com.Movieflix.exception.UsernameOrPasswordInvalidException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class AplicationControlerAdvice {
     @ExceptionHandler(UsernameOrPasswordInvalidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleNotFoundException(UsernameOrPasswordInvalidException ex) {
-        return ex.getMessage();
+    public String handleNotFoundException(UsernameOrPasswordInvalidException e) {
+        return e.getMessage();
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleArgumentNotValidException(MethodArgumentNotValidException ex){
+            Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) ->
+                errors.put(((FieldError) error).getField(), error.getDefaultMessage())
+        );
+        return errors;
     }
 }
